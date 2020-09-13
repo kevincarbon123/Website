f8aaf68cb13db72e64d8a690eebcbfa565386aad
kevincarbon123 <kevin1.tang@ryerson.ca>
2020-09-13 18:24:54 -0400

    deploy

---
+++ precache-manifest.8249d96314b8838dc82da1acdb64f1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe635f60673ccb5fbe05e2615ac501f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea2a292696935e3bf71",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea2a292696935e3bf71",
+    "url": "/static/js/main.ff8d9530.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
